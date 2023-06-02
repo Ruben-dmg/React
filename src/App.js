02cc30f70a998717234704b3db301ea63339f904
Your Name <you@example.com>
2023-06-02 17:38:47 +0200

    Funcional

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
+import Componente1 from './Components/Componente1';
+import Componente2 from './Components/Componente2';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hola</h1>
-    </div>
+    <Router>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Componente 1</Link>
+          </li>
+          <li>
+            <Link to="/otraPagina">Otra página</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Componente1 />} />
+        <Route path="/otraPagina" element={<Componente2 />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/Components/Componente1.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function Componente1() {
+  return (
+    <div>
+      <h1>Componente 1</h1>
+    </div>
+  );
+}
+
+export default Componente1;
