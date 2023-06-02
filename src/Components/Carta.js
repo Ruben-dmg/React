b87f5e2c22ef24afde46506b0a9791ef933d4bee
Your Name <you@example.com>
2023-06-02 22:11:06 +0200

    carta

--- src/Components/Carta.js
+++ src/Components/Carta.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import './style.css';
 
+import Footer from './footer';
+
 function Carta() {
   return (
-    <div>
-      <h1>Carta</h1>
+<div className='wrap'>
+    <div className="marco">
+    <div className="left">
+      <img src='images/image-1.png'  alt="" />
+      <img src='images/image-2.png' alt="" />
+      <img src='images/image-3.png' alt="" />
     </div>
+    <div className="right">
+      <img src='images/image-4.png' alt="" />
+      <img src='images/image-5.png' alt="" />
+      <img src='images/image-6.png' alt="" />
+    </div>
+  </div>
+  <Footer/>
+
+  </div>
   );
 }
 
