ef96bb93230cd446fce8d84816a5b5fa90ffd366
Your Name <you@example.com>
2023-06-02 19:18:42 +0200

    Estructura

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-
-import Componente1 from './Components/Componente1';
-import Componente2 from './Components/Componente2';
+import Home from './Components/Home';
+import Contact from './Components/Contact';
+import Carta from './Components/Carta';
+import './App.css';
 
 function App() {
   return (
     <Router>
       <nav>
-        <ul>
-          <li>
-            <Link to="/">Componente 1</Link>
+        <ul className="navbar">
+          <li className="navbar-item">
+            <Link to="/" className="navbar-link">Inicio</Link>
           </li>
-          <li>
-            <Link to="/otraPagina">Otra página</Link>
+          <li className="navbar-item">
+            <Link to="/contacto" className="navbar-link">Contacto</Link>
+          </li>
+          <li className="navbar-item">
+            <Link to="/carta" className="navbar-link">Nuestra Carta</Link>
           </li>
         </ul>
       </nav>
 
       <Routes>
-        <Route path="/" element={<Componente1 />} />
-        <Route path="/otraPagina" element={<Componente2 />} />
+        <Route path="/" element={<Home/>} />
+        <Route path="/contacto" element={<Contact />} />
+        <Route path="/carta" element={<Carta/>} />
       </Routes>
     </Router>
   );
---
+++ src/Components/Carta.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Carta() {
+  return (
+    <div>
+      <h1>Carta</h1>
+    </div>
+  );
+}
+
+export default Carta;
