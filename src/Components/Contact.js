cf7bb7ed75a32ded07651c38535be712879560bb
Your Name <you@example.com>
2023-06-02 21:30:31 +0200

    Contact

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,11 +1,31 @@
 import React from 'react';
 import './style.css';
 
+import FooterDos from './footer-2';
+
 function Contact() {
   return (
-    <div>
-      <h1>Contacto</h1>
+
+    <div className='wrap'>
+    <div className="page-container">
+    <div className="form-container">
+      <form>
+        <label htmlFor="name">Nombre:</label>
+        <input type="text" id="name" name="name" required />
+
+        <label htmlFor="number">Número:</label>
+        <input type="tel" id="number" name="number" required />
+
+        <label htmlFor="message">Mensaje:</label>
+        <textarea id="message" name="message" required></textarea>
+
+        <button type="submit">Enviar</button>
+      </form>
     </div>
+  </div>
+  <FooterDos/>
+
+  </div>
   );
 }
 
---
+++ src/Components/footer-2.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './style.css';
+
+function FooterDos() {
+  return (
+    <div className='footer-2'>
+      <h3 className='footer-text'>TM & 2023 Veggie Burger. Fictitious brand</h3>
+    </div>
+  )
+}
+
+export default FooterDos
